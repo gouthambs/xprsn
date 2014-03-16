41dcbcb04af51dbfd675cb2e6fb163e248622648
Gouthaman Balaraman <gouthambs@gmail.com>
2014-03-16 15:01:27 -0700

    Added setup notest

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 xprsn
 =====
+
+Set Up Process
+##############
+
+Install virtual environment library 
+```
+pip install virtualenv==1.11.4
+```
+
+Create the virtual env named xprsn-env for development
+```
+virtualenv xprsn-env
+```
+
+Activate the environment
+```
+xprsn-env\Scripts\activate
+```
+
+This will install all the required packages 
+```
+pip install -r Requirements.txt
+```
+
+Create the database
+```python db_create.py```
--- db_create.py
+++ db_create.py
@@ -6,6 +6,7 @@ Created on Sun Mar 16 00:11:33 2014
 """
 
 #!flask/bin/python
+from migrate.exceptions import DatabaseAlreadyControlledError
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
@@ -13,10 +14,15 @@ from webapp import db
 import os.path
 
 
-
-db.create_all()
-if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+try:
+    db.create_all()
+    if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+        api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+        api.version_control(SQLALCHEMY_DATABASE_URI, 
+                            SQLALCHEMY_MIGRATE_REPO)
+    else:
+        api.version_control(SQLALCHEMY_DATABASE_URI, 
+                            SQLALCHEMY_MIGRATE_REPO, 
+                            api.version(SQLALCHEMY_MIGRATE_REPO))
+except DatabaseAlreadyControlledError:
+    print "Oops! Looks like databse already exists!"
