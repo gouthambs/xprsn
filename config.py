769abe8016002a072aad8fa79fc39d075fac267f
Gouthaman Balaraman <gouthambs@gmail.com>
2014-03-16 00:18:31 -0700

    First commit

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 15 23:12:14 2014
+
+@author: Goutham
+"""
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
---
+++ xprsn/gui/routes.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 16:26:25 2014
+
+@author: Goutham
+"""
+
